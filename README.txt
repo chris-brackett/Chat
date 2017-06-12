c4cc96d6af05a3226feaea8c9b33814c440cbf1f
chris-brackett <bracketc@oregonstate.edu>
2017-06-11 19:11:39 -0700

    commit project

---
+++ README.txt
@@ -0,0 +1,19 @@
+CS372 - Project 1 README
+Chris Brackett - bracketc@oregonstate.edu
+
+Instructions:
+1.) Unzip all files into a folder.
+2.) Navigate to that folder in a terminal window.
+3.) Type "chmod +x p1script" without the quotes and hit enter.
+4.) Type "p1script" without the quotes and hit enter.
+5.) Start chatserve by typing "./chatserve <port#>" without quotes and replacing <port#> with a valid port number (i.e. ./chatserve 12468), then hit enter.
+6.) Open a new terminal window (can be on a different flip server) and navigate to the same folder.
+7.) Start chatclient by typing "./chatclient <server-hostname> <port#>" without quotes and replacing <server-hostname> with the server name that chatserve is running on, and <port#> with the port number that chatserve is running on (i.e. ./chatclient flip2.engr.oregonstate.edu 12468), then hit enter.
+
+To use the programs:
+1.) Enter a handle when prompted. It must be 10 characters or less and not contain "\quit".
+2.) Send a message from the client.
+3.) Message will be received by the server, allowing you to send a message from the server.
+4.) Messages must alternate back and forth between client and server.
+5.) To quit, send a message of "\quit" from either the client or the server.
+6.) To terminate chatserve, press ctrl+c.
---
+++ chatclient.c
@@ -0,0 +1,148 @@
+//chatclient
+//Chris Brackett - bracketc@oregonstate.edu
+//CS372 - Project 1
+//2/8/2017
+//Note: code adapted from client.c example from OSU CS344 lecture materials
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+//makeContact
+//This function intitiates contact with the server and returns the socket file descriptor for use later in the program.
+int makeContact(const char * argv[])
+{
+    int socketFD, portNumber;
+    struct sockaddr_in serverAddress;
+    struct hostent* serverHostInfo;
+    
+    // Set up the server address struct - this block of code is mostly from the CS344 lectures
+    memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+    portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
+    serverAddress.sin_family = AF_INET; // Create a network-capable socket
+    serverAddress.sin_port = htons(portNumber); // Store the port number
+    serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address
+    if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+    memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+    
+    // Set up the socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+    if (socketFD < 0) printf("CLIENT: ERROR opening socket\n");
+    
+    // Connect to server
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+    {
+        printf("CLIENT: ERROR connecting to server\n");
+        exit(0);
+    }
+    else
+    {
+        printf("Connected to server.\n");
+    }
+
+    return socketFD;
+}
+
+
+//getHandle
+//This function gets the handle of the user and verifies it is valid.
+void getHandle(char* handle)
+{
+    char* quitCommand = "\\quit\0"; //quit command to compare to handle
+    memset(handle, '\0', 50); //zero out handle
+    
+    printf("What is your handle? "); //get handle from user
+    fflush(stdout);
+    scanf("%s", handle);
+    
+    int handleLength = strlen(handle); //check if handle is more than 10 characters or contains \quit
+    while (handleLength > 10 || strstr(handle, quitCommand) != NULL)
+    {
+        printf("Your handle must be 10 characters or less and not contain \\quit.\n");
+        printf("What is your handle? ");
+        fflush(stdout);
+        memset(handle, '\0', 50); //zero out handle
+        scanf("%s", handle);
+        handleLength = strlen(handle);
+    }
+    
+    handle[handleLength] = '>'; //add > to end of handle
+    handle[handleLength+1] = ' ';
+    
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) { } //clear stdin of newlines - http://stackoverflow.com/questions/7898215/how-to-clear-input-buffer-in-c
+}
+
+
+//sendMessage
+//This function gets input from the user, sends it to the server, and checks whether to quit or not.
+void sendMessage(char* handle, char* buffer, int socketFD)
+{
+    memset(buffer, '\0', 512); // zero out buffer
+    int i;
+    int handleLength = strlen(handle);
+    for (i = 0; i < handleLength; i++)
+    {
+        buffer[i] = handle[i];
+    }
+    
+    printf("%s", handle);
+    
+    fgets(buffer+handleLength, 512-handleLength, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0'; //strip newline off end
+    
+    send(socketFD, buffer, 512, 0); //send message
+    
+    char* quitCommand = "\\quit\0"; //quit command to compare to message
+    if(strstr(buffer, quitCommand) != NULL) //if the quit command was entered by the client
+    {
+        close(socketFD); //close the socket and exit
+        printf("Connection closed.\n");
+        exit(0);
+    }
+}
+
+
+//receiveMessage
+//This function receives the message from the server, prints it to the screen, and checks whether to quit or not.
+void receiveMessage(char* buffer, int socketFD)
+{
+    memset(buffer, '\0', 512); //zero out buffer
+    
+    recv(socketFD, buffer, 512, 0); //get message
+    printf("%s\n", buffer); //print the message
+    
+    char* quitCommand = "\\quit\0"; //quit command to compare to message
+    if(strstr(buffer, quitCommand) != NULL) //if the quit command was entered by the server
+    {
+        close(socketFD); //close the socket and exit
+        printf("Connection closed.\n");
+        exit(0);
+    }
+}
+
+
+int main(int argc, const char * argv[]) {
+    if (argc != 3) { fprintf(stderr,"USAGE: %s chatclient <server-hostname> <port#>\n", argv[0]); exit(1); } // Check usage & args
+    
+    int socketFD = makeContact(argv); //make contact, get socket file descriptor
+
+    char buffer[512]; //buffer for holding messages being sent and received
+    
+    char handle[50]; //holds handle of user
+    getHandle(handle); //get handle from user
+    
+    while(1)
+    {
+        sendMessage(handle, buffer, socketFD); //send message
+        receiveMessage(buffer, socketFD); //receieve message
+    }
+
+    return 0;
+}
